eadd9576150be1ac9aae7134fba98cb6aaf93539
Hilalyilmaz1 <yilmazhilal053gmail.com>
2026-07-07 19:46:35 +0300

    LLM updated

--- app/ai/llm.py
+++ app/ai/llm.py
@@ -39,7 +39,7 @@ class LLMWrapper:
         elif self.provider == "groq":
             try:
                 response = self.client.chat.completions.create(
-                    model="llama3-8b-8192",
+                    model="llama-3.1-8b-instant",
                     messages=[{"role": "user", "content": prompt}],
                     temperature=0.5
                 )
